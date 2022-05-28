10b00a9396f59b4d573dbd7cfcf42846fa1ff61d
noormohammad20 <mdsaim204@gmail.com>
2022-05-28 22:23:17 +0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import Countries from './components/Countries'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Visiting Countries</Text>
+      <Countries></Countries>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ components/Countries.js
@@ -0,0 +1,26 @@
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import Country from './Country'
+
+
+export default function Countries() {
+    const [countries, setCountries] = useState([])
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+    return (
+        <View>
+            <Text>Countries: {countries.length}</Text>
+            <ScrollView>
+                {
+                    countries.map(country => <Country
+                        key={country.id}
+                        country={country}
+                    ></Country>)
+                }
+            </ScrollView>
+        </View>
+    )
+}

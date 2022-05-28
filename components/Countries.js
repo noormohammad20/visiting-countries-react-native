430c6c0a854750ccd42053915c6878273aac7feb
noormohammad20 <mdsaim204@gmail.com>
2022-05-28 23:20:42 +0600

    Display image in a Scrollview and implement search

--- components/Countries.js
+++ components/Countries.js
@@ -1,21 +1,35 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TextInput } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Country from './Country'
 
 
 export default function Countries() {
     const [countries, setCountries] = useState([])
+    const [searched, setSearched] = useState([])
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
-            .then(data => setCountries(data))
+            .then(data => {
+                setCountries(data)
+                setSearched(data)
+            })
     }, [])
+    const handleSearched = text => {
+        const filtered = countries.filter(country => country.name.common.includes(text))
+        setSearched(filtered)
+    }
     return (
         <View>
-            <Text>Countries: {countries.length}</Text>
+            <Text style={style.header}>Countries: {countries.length}</Text>
+
+            <TextInput
+                onChangeText={handleSearched}
+                style={style.input}
+            ></TextInput>
+
             <ScrollView>
                 {
-                    countries.map(country => <Country
+                    searched.map(country => <Country
                         key={country.id}
                         country={country}
                     ></Country>)
@@ -24,3 +38,17 @@ export default function Countries() {
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    header: {
+        marginTop: 50,
+        fontSize: 40,
+        color: 'red',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+})
--- components/Country.js
+++ components/Country.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 export default function Country({ country }) {
     return (
         <View>
-            <Text>Country{country.name.common}</Text>
+            <Text style={{ fontSize: 20 }}>Country{country.name.common}</Text>
+            <Image
+                source={{
+                    uri: country.flags.png
+                }}
+                style={{ height: 200, width: 200 }}
+            >
+            </Image>
         </View>
     )
 }
+
